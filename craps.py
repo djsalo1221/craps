75a1d207f326d38118b3680f69e63963245c6ca6
Daniel Salo <ds336923@wne.edu>
2014-10-01 16:42:08 -0400

    We fixed the variable names to our liking, and kept the comments

--- craps.py
+++ craps.py
@@ -1,26 +1,26 @@
 from random import randint           #random, randint
 
 print('come-out')               # vague output
-r1 = randint(1, 6)               #vague variable name
-r2 = randint(1, 6)                #vague variable name
-v = r1 + r2                 #vague assignment
-print(v)                            #vague variable
-if v == 7 or v == 11:
+firstDie = randint(1, 6)               #vague variable name
+secondDie = randint(1, 6)                #vague variable name
+firstRoll = firstDie + secondDie                 #vague assignment
+print(firstRoll)                            #vague variable
+if firstRoll == 7 or firstRoll == 11:
     print('You win!')                       
-elif v == 2 or v == 3 or v == 12:
+elif firstRoll == 2 or firstRoll == 3 or firstRoll == 12:
     print('Craps, you lose.')       
 else:
-    print('The point is', v)      # vague variable name
-    r1 = randint(1, 6)          #vague variable name
-    r2 = randint(1, 6)         #vague variable name
-    w = r1 + r2                #vague assignment
-    print(w)
-    while w != v and w != 7:
-        r1 = randint(1, 6)       #vague variable name
-        r2 = randint(1, 6)       #vague variable name
-        w = r1 + r2
-        print(w)
-    if w == 7:
+    print('The point is', firstRoll)      # vague variable name
+    firstDie = randint(1, 6)          #vague variable name
+    secondDie = randint(1, 6)         #vague variable name
+    secondRoll = firstDie + secondDie                #vague assignment
+    print(secondRoll)
+    while secondRoll != firstRoll and secondRoll != 7:
+        firstDie = randint(1, 6)       #vague variable name
+        secondDie = randint(1, 6)       #vague variable name
+        secondRoll = firstDie + secondDie
+        print(secondRoll)
+    if secondRoll == 7:
         print('Seven out, you lose.')
     else:
         print('Pass, you win!')
