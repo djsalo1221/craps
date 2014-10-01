f100fdac25e0848b06832c8868fd736b642666bd
Daniel Salo <ds336923@wne.edu>
2014-10-01 16:34:33 -0400

    We identified the "Bad"identifiers with comments

--- craps.py
+++ craps.py
@@ -1,23 +1,23 @@
-from random import randint
+from random import randint           #random, randint
 
-print('come-out')
-r1 = randint(1, 6)
-r2 = randint(1, 6)
-v = r1 + r2
-print(v)
+print('come-out')               # vague output
+r1 = randint(1, 6)               #vague variable name
+r2 = randint(1, 6)                #vague variable name
+v = r1 + r2                 #vague assignment
+print(v)                            #vague variable
 if v == 7 or v == 11:
-    print('You win!')
+    print('You win!')                       
 elif v == 2 or v == 3 or v == 12:
-    print('Craps, you lose.')
+    print('Craps, you lose.')       
 else:
-    print('The point is', v)
-    r1 = randint(1, 6)
-    r2 = randint(1, 6)
-    w = r1 + r2
+    print('The point is', v)      # vague variable name
+    r1 = randint(1, 6)          #vague variable name
+    r2 = randint(1, 6)         #vague variable name
+    w = r1 + r2                #vague assignment
     print(w)
     while w != v and w != 7:
-        r1 = randint(1, 6)
-        r2 = randint(1, 6)
+        r1 = randint(1, 6)       #vague variable name
+        r2 = randint(1, 6)       #vague variable name
         w = r1 + r2
         print(w)
     if w == 7:
